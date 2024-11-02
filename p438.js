cd99aae0b336ca24c9236d58a8ee0810a487b24e
Gabriela Sanchez <gabysan0487@gmail.com>
2024-11-01 23:09:26 -0300

    practicas

--- p438.js
+++ p438.js
@@ -68,6 +68,7 @@ console.log(resultPerson(namePerson));
 
 // Salida: ["Ana", "Juan", "Luis"]
 
+
 // 6. Convertir un objeto anidado en un array de sus valores
 // Objetivo: Convertir todos los valores, incluyendo los anidados, en un array.
 
@@ -77,7 +78,7 @@ const personAnid = {
 };
 
 const nestedObject = (object) => {
-  return Object.values(object);
+  return Object.values(object); //le falta 
 };
 console.log(nestedObject(personAnid));
 // Salida: [["nombre", "Luis"], ["edad", 30], "Ingeniero"]
@@ -87,7 +88,9 @@ console.log(nestedObject(personAnid));
 Entrada: [{ precio: 20 }, { precio: 15 }, { precio: 10 }];
 
 const sumOfValues = (arr) => {
-  return arr.map((item) => item.precio).reduce((acc, curr) => acc + curr, 0);
+  return arr
+  .map((item) => item.precio)
+  .reduce((acc, curr) => acc + curr, 0);
 };
 console.log(sumOfValues([{ precio: 20 }, { precio: 15 }, { precio: 10 }]));
 
@@ -97,7 +100,14 @@ Salida: 45;
 // Objetivo: Eliminar la propiedad edad de cada objeto y luego ordenar por nombre.
 // Entrada: [{nombre: "Juan", edad: 21}, {nombre: "Ana", edad: 20}]
 
+``
 
+const deleteProp = (arr) => {
+  return arr
+  .map(({nombre}) => ({nombre}))
+  .sort((a, b) => a.nombre.localeCompare(b.nombre))
+}
+console.log(deleteProp([{nombre: "Juan", edad: 21}, {nombre: "Ana", edad: 20}]))
 // Salida: [{nombre: "Ana"}, {nombre: "Juan"}]
 
 // 9. Filtrar y mapear un array de strings
@@ -116,16 +126,40 @@ console.log(filterWords(["Hola", "Mundo", "JavaScript", "JS", "Code"]));
 // Objetivo: Crear un objeto que cuente la frecuencia de cada fruta en el array.
 // Entrada: ["manzana", "pera", "manzana", "naranja", "pera", "manzana"]
 
+
+const countElement = (arr) => {
+return arr.reduce((acc, curr) => {
+  acc[curr] = (acc[curr] || 0 ) + 1
+return acc
+},{})
+}
+console.log(countElement(["manzana", "pera", "manzana", "naranja", "pera", "manzana"]))
 // Salida: { manzana: 3, pera: 2, naranja: 1 }
+
 
 // 11. Combinar y transformar propiedades de objetos
 // Objetivo: Crear un array de strings combinando nombre y edad en el formato "nombre (edad)".
 // Entrada: [{nombre: "Luis", edad: 18}, {nombre: "Ana", edad: 22}]
 
+const combineAndTransform = (arr) => {
+  return arr.map((item) => `${item.nombre} (${item.edad})`)
+} 
+console.log(combineAndTransform([{nombre: "Luis", edad: 18}, {nombre: "Ana", edad: 22}]))
 // Salida: ["Luis (18)", "Ana (22)"]
+
 
 // 12. Obtener propiedades de objetos en un array y contar valores específicos
 // Objetivo: Crear un objeto que cuente cuántos de cada tipo hay en el array.
 // Entrada: [{tipo: "auto"}, {tipo: "moto"}, {tipo: "auto"}, {tipo: "camión"}]
+
+
+const createAnObjectThatCounts = (arr) => {
+
+  return arr.reduce((acc, curr) => {
+    acc[curr.tipo] = (acc[curr.tipo] || 0 ) + 1 
+return  acc
+  },{})
+}
+console.log(createAnObjectThatCounts([{tipo: "auto"}, {tipo: "moto"}, {tipo: "auto"}, {tipo: "camión"}]))
 
 // Salida: { auto: 2, moto: 1, camión: 1 }
