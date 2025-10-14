9832bb4abac0e484b2e385c8f83308b3ae94d685
Gabriela Sanchez <gabysan0487@gmail.com>
2025-10-13 23:04:26 -0300

    practica

--- p493.js
+++ p493.js
@@ -17,16 +17,19 @@
 
 // Con un bucle for...of y Map.
 
-const fruit = ["manzana", "pera", "manzana", "uva", "pera", "manzana"];
+// +
 
 const countWords = (arraywords) => {
   return arraywords.reduce((acc, curr) => {
-    // acc[curr] = acc[curr] ? acc[curr] + 1 : 1;
-    acc[curr] = (acc[curr] || 0) + 1;
+    acc[curr] = acc[curr] ? acc[curr] + 1 : 1;
+    // acc[curr] = (acc[curr] || 0) + 1
     return acc;
   }, {});
 };
-console.log(countWords(fruit));
+console.log(
+  countWords(["manzana", "pera", "manzana", "uva", "pera", "manzana"])
+);
+// console.log(countWords(fruit));
 
 // Diseña un algoritmo que determine si una palabra es un palíndromo, es decir, una palabra que se lee igual de izquierda a derecha que de derecha a izquierda.
 // Entrada: "reconocer"
@@ -37,8 +40,20 @@ console.log(countWords(fruit));
 // "Es palíndromo"
 
 const isPalindrome = (word) => {
-  return word === word.split("").reverse().join("")
+  return word.split("").reverse().join("") === word
     ? "Es palíndromo"
     : "No es palíndromo";
 };
 console.log(isPalindrome("reconocer"));
+console.log(isPalindrome("oso"));
+
+//Sumar todos los números de un arreglo
+
+// const number = [1, 2, 3, 4, 5];
+
+// salida : 15
+
+const sumArray = (array) => {
+  return array.reduce((acc, curr) => acc + curr, 0);
+};
+console.log(sumArray([1, 2, 3, 4, 5]));
